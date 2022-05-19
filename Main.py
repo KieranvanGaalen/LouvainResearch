31d285f8a6901e4d52b93edde6b35a9e962d5204
Joes <j.dejonge@students.uu.nl>
2022-05-19 14:24:23 +0200

    setup with correct degrees

--- Main.py
+++ Main.py
@@ -2,10 +2,9 @@ import matplotlib.pyplot as pp
 import networkx as nx
 
 fastGNPNetwork = nx.fast_gnp_random_graph(100, 0.05)
-information = nx.path_graph(10)
 
-print(information.degree[0])
+print(fastGNPNetwork.degree[0])
 
-nx.draw(fastGNPNetwork, node_color=['blue','red'], with_labels=True)
+nx.draw(fastGNPNetwork, node_color=['blue'] + ['red'] * (99), with_labels=True)
 
 pp.show()
