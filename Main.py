bb37309b9670eb75a2c7eb71dfff83c1953b606d
Kieran van Gaalen <kieran.van.gaalen@casema.nl>
2022-06-09 14:09:11 +0200

    loops in main.py

--- Main.py
+++ Main.py
@@ -7,27 +7,48 @@ import time
 
 seed = 5 # TODO: Set this
 
-n = 100
+def getSizes(numcom : int) -> List[int]:
+    sizes = []
+    for i in range(numcom):
+        sizes.append(random.randrange(1, 100))
+    return sizes
 
-k = {0 : [1,2,3], 1 : [0,2,3], 2 : [0,1,3], 3 : [0,1,2,5,9], 4: [5,6,7], 5 : [3,4,6,7,12], 6: [4,5,7], 7 : [4,5,6], 8 : [9,10,11], 9 : [8,10,11,3,12], 10 : [8,9,11], 11: [8,9,10], 12:[5,9,13,14,15], 13: [12,14,15], 14: [12,13,15], 15 : [12,14,13]}
-graph = nx.from_dict_of_lists(k)
-fastGNPNetwork = nx.barbell_graph(50, 0)
+def getProbs(numcom : int) -> List[List[int]]:
+    probs = []
+    for i in range(numcom):
+        tmplist = []
+        for j in range(numcom):
+            tmplist.append(0)
+        probs.append(tmplist)
+    for i in range(numcom):
+        for j in range(i, numcom):
+            val = random.uniform(0,1)
+            probs[i][j] = val
+            probs[j][i] = val
+    return probs
 
 runtimes = [] # Use this for runtime analysis
+edgeCounts = []
+nodeCounts = []
+foundComs = []
+numcoms = []
 for _ in range(10):
-    sizes = [75, 75, 300] # TODO: Randomise this based on seed
-    probs = [[0.25, 0.05, 0.02], [0.05, 0.35, 0.07], [0.02, 0.07, 0.40]] # TODO: Randomise this based on seed
-    stochasticGraph = nx.stochastic_block_model(sizes, probs, seed)
+    numcom = random.randrange(1, 50)
+    sizes = getSizes(numcom)
+    probs = getProbs(numcom)
+    print(numcom)
+    print(sizes)
+    print(probs)
+    stochasticGraph = nx.stochastic_block_model(sizes, probs)
 
+    numcoms.append(numcom)
+    edgeCounts.append(stochasticGraph.number_of_edges())
+    nodeCounts.append(stochasticGraph.number_of_nodes())
     start = time.time()
-    commDict = louvain_getCommunities(fastGNPNetwork.copy(), EdgeRatio.Measure())
+    commDict = louvain_getCommunities(stochasticGraph.copy(), EdgeRatio.Measure())
     end = time.time()
     runtimes.append(end - start)
-
-    #print("runtime: " + str(end - start))
-    #print(commDict)
+    foundComs.append(len(commDict)
     # TODO: Ground truth analysis
 
-nx.draw(fastGNPNetwork, node_color=['blue'] + ['red'] * (99), with_labels=True)
 
-pp.show()
